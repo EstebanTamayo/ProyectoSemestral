623d1ce3e69cdf441d67c30d6c5564101436c888
Mauricio Herrera Ortega <ronalli56@gmail.com>
2022-11-08 12:06:42 -0300

    Avance conexion base de datos

--- src/proyectosemestral/TestData.java
+++ src/proyectosemestral/TestData.java
@@ -4,10 +4,12 @@
  */
 package proyectosemestral;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import javax.swing.UIManager;
 
 /**
  *
@@ -15,8 +17,8 @@ import java.sql.Statement;
  */
 public class TestData extends javax.swing.JFrame {
 
-    private String stringDB = "";
-    private String user = "";
+    private String stringDB = "jdbc:mysql://localhost:3306/test";
+    private String user = "root";
     private String pass = "";
     
     /**
@@ -69,8 +71,9 @@ public class TestData extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         try {
-            String sql = "";
+            String sql = "SELECT * FROM tab";
             Connection connection = DriverManager.getConnection(stringDB, user,pass);
+            System.out.println(connection);
             Statement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery(sql);
             
@@ -79,7 +82,8 @@ public class TestData extends javax.swing.JFrame {
             // else System.out.println("Choose id again");
             
             if(resultSet.next()){
-                String campo1 = resultSet.getString(1);
+                String campo1 = resultSet.getString(2);
+                System.out.println(campo1);
             }
             else{
                 System.out.println("No data found");
@@ -95,27 +99,11 @@ public class TestData extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TestData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TestData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TestData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TestData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+                try{
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+} catch( Exception ex ) {
+    System.err.println( "Failed to initialize LaF" );
+}
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
